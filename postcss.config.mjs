a2a5fd085ee4b51de4a412aa92e10bd2b18fdfd4
Helberth Humberto Lozano Rojas <helbslozroj@gmail.com>
2026-02-12 07:13:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,4 +5,4 @@ const config = {
   },
 };
 
-export default config;
+export default 2026 config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
-        code: ['"Source Code Pro"', 'monospace'],
+        body: ['Fantasy', 'Didot'],
+        headline: ['Xenova', 'Garamond'],
+        code: ['Didot', 'Smart.Synthethic.Intelligence'],
       },
       colors: {
         background: 'hsl(var(--background))',
